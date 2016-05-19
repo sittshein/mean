0f1d5f7c2c7a229e353d20c2384953a464aba282
John Smith <sittshein.app.developer@gmail.com>
2016-05-19 17:45:26 +0800

    include dashboard

--- client/app/admin/admin.router.ts
+++ client/app/admin/admin.router.ts
@@ -3,11 +3,21 @@
 angular.module('meanFullstackApp.admin')
   .config(function($stateProvider) {
     $stateProvider
+
       .state('admin', {
         url: '/admin',
         templateUrl: 'app/admin/admin.html',
         controller: 'AdminController',
         controllerAs: 'admin',
         authenticate: 'admin'
+      })
+
+      .state('dashboard', {
+        url: '/dashboard',
+        templateUrl: 'app/admin/dashboard/dashboard-main.html',
+        controller: 'AdminController',
+        controllerAs: 'dashboard',
+        authenticate: 'dashboard'
       });
+
   });
